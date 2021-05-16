8f2801927f17f6f1b1dec364917cfd379e44544c
Shinchan7222 <83214906+Shinchan7222@users.noreply.github.com>
2021-05-16 21:27:48 +0530

    Update helper.py

--- handlers/helper.py
+++ handlers/helper.py
@@ -32,3 +32,18 @@ async def helper(ok, message: Message):
 @Client.on_message(command("help") & other_filters)
 async def ghelp(_, message: Message):
     await message.reply_text(f"**{bn} :-** Hey! DM me to get all the commands 😉")
+                            reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Click here to Dm me for help😏", url="http://t.me/MAFIA_MUSIC_ROBOT?start=help"
+                    )
+                ],[
+                    InlineKeyboardButton(
+                        "Support Group 🎙️", url="https://t.me/MafiaBot_Chit_Chat""
+                    ),
+                    )
+                ]
+            ]
+        )
+    )
