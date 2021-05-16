c7a750f1587a00993b1a011ea645e43d4c8a1d26
Shinchan7222 <83214906+Shinchan7222@users.noreply.github.com>
2021-05-17 00:02:17 +0530

    Update Ghelper.py

--- handlers/Ghelper.py
+++ handlers/Ghelper.py
@@ -4,6 +4,8 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from config import BOT_NAME as bn
 from helpers.filters import command, other_filters2, other_filters
 
+
+
 @Client.on_message(command("help") & other_filters)
 async def ghelp(_, message: Message):
     await message.reply_text(f"**{bn} :-** Hey! DM me to get all the commands 😉")
