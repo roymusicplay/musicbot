8d45f4c874b64d784c7b08104570f906239b4c76
D3_krish <83129000+D3KRISH@users.noreply.github.com>
2021-05-26 17:27:04 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -7,7 +7,7 @@ from helpers.filters import other_filters2
 
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
-    await message.reply_sticker("")
+    await message.reply_sticker("CAACAgUAAxkBAAIZFWCquxoG_Ervt_4d61DnSF3YKLI9AALGAgAClA5RVUG4_pmGDtAXHwQ")
     await message.reply_text(
         f"""**Hey, I'm {bn} 🎵
 
